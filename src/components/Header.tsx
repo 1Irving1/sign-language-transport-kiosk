6b06847731404b38afa99dfd9d85a71bc3a56729
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2025-11-13 23:07:41 +0900

    refactor: Header 컴포넌트 뒤로가기/홈 버튼 및 타이틀 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,25 @@
-export default function Header() {
+import HomeIcon from "./icons/HomeIcon";
+import BackIcon from "./icons/backIcon";
+
+interface HeaderProps {
+  title: string;
+}
+
+
+export default function Header({ title }: HeaderProps) {
   return (
-    <header className="bg-blue-100 w-full py-10 text-center shadow-sm">
+    <header className="bg-blue-50 w-full py-6 shadow-sm relative">
+      <button className="absolute left-4 top-1/2 -translate-y-1/2">
+        <BackIcon/>
+      </button>
+      
+     
+      <h1 className="text-center font-bold text-lg">{title}</h1>
+      
+     
+      <button className="absolute right-4 top-1/2 -translate-y-1/2">
+        <HomeIcon/>
+      </button>
     </header>
   );
 }
