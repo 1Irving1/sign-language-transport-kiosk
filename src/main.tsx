e60b4999ace43330b315ec4b6edab9b8ffc31278
pmh0712 <137433778+pmh0712@users.noreply.github.com>
2025-11-18 15:22:17 +0900

    카메라 인식을 위한 StrictMode제거
    이거 안하면 카메라 안됌

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+//StrictMode의 문제가 있는지 검사하기 위해 두 번 실행하는 것으로 오류 발생, 주석 처리함
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  //<StrictMode>
     <App />
-  </StrictMode>,
+  //</StrictMode>,
 )
+
