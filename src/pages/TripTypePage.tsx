0c0cffdafaa396433820f350adca99253bb62fb5
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2025-11-19 14:27:26 +0900

    refactor:편도,왕복인지 날짜/시간대 화면으로 상태 넘겨주기

--- src/pages/TripTypePage.tsx
+++ src/pages/TripTypePage.tsx
@@ -1,37 +1,48 @@
-import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
-
-
 
 export default function TripTypePage() {
 
   const navigate = useNavigate();
 
-  const handleStart = () => {
-    navigate("/datatime"); // 날짜 화면 이동
+  const handleStart = (type: "one-way" | "round") => {
+    navigate("/datetime", {
+      state: { tripType: type },
+    });
   };
 
   return (
     <div className="flex justify-center w-screen h-screen bg-white to-gray-100">
       <div className="w-[450px] h-[900px] bg-gradient-to-b from-blue-50 to-white shadow-xl shadow-2xl flex flex-col">
-      <Header title="여행 종류 선택"/>
+
+        <Header title="여행 종류 선택" />
+
         <main className="mt-8 px-6">
           <p className="text-xl font-bold">어떤 방식으로 여행하시나요?</p>
-          <p>편도 또는 왕복 중에서 선택해주세요.</p> 
+          <p>편도 또는 왕복 중에서 선택해주세요.</p>
         </main>
 
-   
-          
         <div className="flex flex-row items-center gap-6 mt-6 mx-auto">
-          <button onClick = {handleStart}
-          className="w-[180px] h-[200px]  bg-blue-300 text-white hover:bg-blue-400 rounded-2xl shadow-md "
-          >편도로 갈래요.</button>
-          <button onClick = {handleStart}
-          className="w-[180px] h-[200px] bg-yellow-100 text-yellow-700 hover:bg-yellow-200 border border-yellow-300 rounded-2xl shadow-md">왕복으로 갈래요.</button>
+
+          {/* 편도 버튼 */}
+          <button
+            onClick={() => handleStart("one-way")}
+            className="w-[180px] h-[200px] bg-blue-300 text-white hover:bg-blue-400 rounded-2xl shadow-md"
+          >
+            편도로 갈래요.
+          </button>
+
+          {/* 왕복 버튼 */}
+          <button
+            onClick={() => handleStart("round")}
+            className="w-[180px] h-[200px] bg-yellow-100 text-yellow-700 hover:bg-yellow-200 border border-yellow-300 rounded-2xl shadow-md"
+          >
+            왕복으로 갈래요.
+          </button>
+
         </div>
 
-        <div className="w-[80%]  bg-white border border-gray-200 px-4 py-3 mb-5 shadow-sm mt-6 mx-auto">
+        <div className="w-[80%] bg-white border border-gray-200 px-4 py-3 mb-5 shadow-sm mt-6 mx-auto">
           <p className="text-sm text-gray-700">
             <span className="font-semibold text-gray-900">출발역:</span> 부산역
           </p>
@@ -39,11 +50,8 @@ export default function TripTypePage() {
             <span className="font-semibold text-gray-900">도착역:</span> 서울역
           </p>
         </div>
+
       </div>
     </div>
-    );
+  );
 }
-
-
-
-
